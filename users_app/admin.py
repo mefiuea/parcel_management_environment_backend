09e692cabca244b7a6de119030cecf790bcabd16
Maciej Koczot <mefiuea@gmail.com>
2022-08-18 16:48:06 +0200

    add my user model to admin panel

--- users_app/admin.py
+++ users_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'user_name', 'last_name', 'is_admin', 'date_joined', 'last_login',)
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        ('Personal info', {'fields': ('user_name', 'first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active', 'is_staff', 'is_superuser', 'is_email_verified')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(get_user_model(), CustomUserAdmin)
