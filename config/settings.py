eeb57744f1c87c1f9a5d47fd0c56a2773cfa0d77
Maciej Koczot <mefiuea@gmail.com>
2022-08-18 15:18:41 +0200

    create custom user model

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # 3-rd party
+
+    # local
+    'users_app.apps.UsersAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -70,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -85,7 +87,6 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -117,7 +117,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -127,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users_app.MyUser'
--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, user_name, password=None):
+        if not email:
+            raise ValueError('User must provide email.')
+        if not user_name:
+            raise ValueError('User must provide user name.')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            user_name=user_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, user_name, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            user_name=user_name,
+            password=password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.is_email_verified = True
+        user.save(using=self._db)
+
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    user_name = models.CharField(max_length=30, unique=False, verbose_name='user name')
+    first_name = models.CharField(max_length=30, unique=False, verbose_name='first name')
+    last_name = models.CharField(max_length=30, unique=False, verbose_name='last name')
+    email = models.EmailField(max_length=100, unique=True, verbose_name='email')
+    is_email_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', ]
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
