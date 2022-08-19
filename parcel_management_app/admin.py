1918419696fd4eff0a2c2c4880a6dceaa25666f2
Maciej Koczot <mefiuea@gmail.com>
2022-08-19 13:38:06 +0200

    Register Parcel and ParcelShelf models to Admin panel

--- parcel_management_app/admin.py
+++ parcel_management_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Parcel, ParcelShelf
+
+
+@admin.register(Parcel)
+class ParcelAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'creation_date', 'last_modification')
+
+
+@admin.register(ParcelShelf)
+class ParcelShelfAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'creation_date', 'last_modification')
+
