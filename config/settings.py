7d88c5ce3d9b446bb455adf7f46e082749456332
Maciej Koczot <mefiuea@gmail.com>
2022-08-20 14:07:27 +0200

    configure email backend

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,12 @@ REST_FRAMEWORK = {
     #     'rest_framework.authentication.TokenAuthentication',
     # ],
 }
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_APP_PASSWORD')
